3d9315633a70a3d52b8bf119616abac34f6676eb
therockstarind <rohitkumarrohit96@gmail.com>
2025-03-12 01:16:42 +0530

    feat: remove gallery components and related files to simplify the project structure

--- src/wip/comp/gallery/preview/index.tsx
+++
@@ -1,7 +0,0 @@
-import { DriveItem } from '®/types/drive'
-
-import ImagePreview from './image'
-
-export function Preview({ c, isImage }: { c: DriveItem; isImage: boolean }) {
-    return <>{isImage ? <ImagePreview c={c} /> : <>video {c.name}</>}</>
-}
