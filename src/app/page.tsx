fd2d7c60b7aabf19f63b7189a0da19f132274bd9
therockstarind <rohitkumarrohit96@gmail.com>
2025-03-12 18:31:41 +0530

    feat: simplify Page component structure and update return statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 'use client'
 
-import { Firmware, FRP, Quotes } from '®components/showcase'
-
 export default function Page() {
-    return (
-        <main className='mx-auto space-y-10 p-1 py-4 pb-10 sm:p-4 2xl:px-[10%]'>
-            <Firmware />
-            <FRP />
-            <Quotes />
-        </main>
-    )
+    return <main className='mx-auto space-y-10 p-1 py-4 pb-10 sm:p-4 2xl:px-[10%]'>hey</main>
 }
