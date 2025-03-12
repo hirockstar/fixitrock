86d8167c77968725bb22f385c560ca5ea217738e
therockstarind <rohitkumarrohit96@gmail.com>
2025-03-12 21:29:41 +0530

    refactor: rename Drive component to Page and simplify implementation

--- src/app/drive/page.tsx
+++ src/app/drive/page.tsx
@@ -1,11 +1,5 @@
 'use client'
 
-import { Grid } from './ui'
-
-import { useDrive } from '®tanstack/query'
-
-export default function Drive() {
-    const { data, isLoading, selectItem } = useDrive('')
-
-    return <Grid data={data} isLoading={isLoading} onSelect={selectItem} />
+export default function Page() {
+    return <h1>test</h1>
 }
