16d6fa68d7e279b3a097b78605a74654bd5eec47
therockstarind <rohitkumarrohit96@gmail.com>
2025-03-13 19:44:43 +0530

    feat: add getID function to drive actions and implement GET route for file download

--- src/actions/drive/index.ts
+++ src/actions/drive/index.ts
@@ -1,8 +1,9 @@
 import { getChildren } from './children'
+import { getID } from './id'
 import { getMeta } from './meta'
 import { getReadme } from './readme'
 import { getSearch } from './search'
 import { getStorage } from './storage'
 import { getToken } from './token'
 
-export { getChildren, getMeta, getReadme, getSearch, getStorage, getToken }
+export { getChildren, getMeta, getReadme, getSearch, getStorage, getToken, getID }
