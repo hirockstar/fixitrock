f2b712adf581467f72b19906bc7cd5795217edc4
A Team <ara6i.sn@gmail.com>
2025-05-13 14:58:40 +0300

    fix build  issue

--- src/app/(user)/rockstar/page.tsx
+++ src/app/(user)/rockstar/page.tsx
@@ -1,12 +1,15 @@
 import Profile from '../ui/profile'
 import Tabs from '../ui/tabs'
+import { Suspense } from 'react'
 
 export default function Page() {
     return (
         <main>
             <Profile />
             <div className='mx-auto -mt-12 p-1 2xl:px-[10%]'>
-                <Tabs />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Tabs />
+                </Suspense>
             </div>
         </main>
     )
